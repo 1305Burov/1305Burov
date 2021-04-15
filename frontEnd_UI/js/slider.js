8b9ba1f286f53589f670286505db4e2ef729fc4e
1305Burov <53432056+1305Burov@users.noreply.github.com>
2021-04-15 21:22:27 +0300

    Add files via upload

---
+++ frontEnd_UI/js/slider.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('.slider').slick({
+  	dots: true,
+  	responsive: [
+    {
+      breakpoint: 840,
+      settings: {
+        arrows: false,
+        
+      }
+    }
+    ]
+
+    });
+});
